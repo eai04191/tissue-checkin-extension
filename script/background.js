668d0c4b325002a37833aeae78245630228241d6
Irie Aoi <eai@mizle.net>
2020-05-30 07:32:45 +0900

    lint

--- script/background.js
+++ script/background.js
@@ -1,4 +1,4 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
+chrome.browserAction.onClicked.addListener(function (tab) {
     chrome.tabs.create({
         url:
             "https://shikorism.net/checkin?link=" + encodeURIComponent(tab.url),
