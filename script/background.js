0702abd8d04cba57813ca58aa8772d22ab9b7c4b
Irie Aoi <eai@mizle.net>
2020-05-30 09:55:55 +0900

    Fanza同人とDLsite Playに対応

--- script/background.js
+++ script/background.js
@@ -1,6 +1,40 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
-    chrome.tabs.create({
-        url:
-            "https://shikorism.net/checkin?link=" + encodeURIComponent(tab.url),
-    });
+    const url = tab.url;
+    checkin(parseUrl(url));
 });
+
+function checkin(url) {
+    chrome.tabs.create({
+        url: "https://shikorism.net/checkin?link=" + encodeURIComponent(url),
+    });
+}
+
+function parseUrl(url) {
+    return parseFanzaDoujinUrl(url) || parseDLsitePlayUrl(url) || url;
+}
+
+function parseFanzaDoujinUrl(url) {
+    if (
+        !/^https:\/\/www\.dmm\.co\.jp\/dc\/-\/(mylibrary\/detail|viewer)\/=/.test(
+            url
+        )
+    ) {
+        return false;
+    }
+    console.log(url, "is Fanza Url.");
+    const id = url.match(
+        /^https:\/\/www\.dmm\.co\.jp\/dc\/-\/(mylibrary\/detail|viewer)\/=\/product_id=(?<id>._\d+)/
+    ).groups.id;
+    return `https://www.dmm.co.jp/dc/doujin/-/detail/=/cid=${id}/`;
+}
+
+function parseDLsitePlayUrl(url) {
+    if (!/^https:\/\/play\.dlsite\.com\/#/.test(url)) {
+        return false;
+    }
+    console.log(url, "is DLsite Play Url.");
+    const id = url.match(/^https:\/\/play\.dlsite\.com\/#\/work\/(?<id>..\d+)/)
+        .groups.id;
+
+    return `https://www.dlsite.com/maniax/work/=/product_id/${id}.html`;
+}
