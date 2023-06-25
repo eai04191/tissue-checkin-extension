45f6394303e3d778b429b36b47a9aa7bf0a246c9
Irie Aoi <eai@mizle.net>
2023-06-26 00:01:07 +0900

    deps: upgrade dependencies (latest)

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,34 +1,29 @@
-import { ConfigurationFactory } from "webpack";
+import { type Configuration } from "webpack";
 import path from "path";
 import CopyWebpackPlugin from "copy-webpack-plugin";
 
-// https://qiita.com/markey/items/ea9ed18a1a243b39e06e
-const config: ConfigurationFactory = () => {
-    return {
-        entry: {
-            // content_scripts: path.join(__dirname, "src", "content_scripts.ts"),
-            background: path.join(__dirname, "src", "background.ts"),
-        },
-        output: {
-            path: path.join(__dirname, "dist"),
-            filename: "[name].js",
-        },
-        module: {
-            rules: [
-                {
-                    test: /.ts$/,
-                    use: ["babel-loader", "ts-loader"],
-                    exclude: "/node_modules/",
-                },
-            ],
-        },
-        resolve: {
-            extensions: [".ts", ".js"],
-        },
-        plugins: [
-            new CopyWebpackPlugin({ patterns: [{ from: "public", to: "." }] }),
+const config: Configuration = {
+    entry: {
+        background: path.join(__dirname, "src", "background.ts"),
+    },
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: "[name].js",
+    },
+    module: {
+        rules: [
+            {
+                test: /.ts$/,
+                use: ["babel-loader", "ts-loader"],
+            },
         ],
-    };
+    },
+    resolve: {
+        extensions: [".ts", ".js"],
+    },
+    plugins: [
+        new CopyWebpackPlugin({ patterns: [{ from: "public", to: "." }] }),
+    ],
 };
 
 export default config;
