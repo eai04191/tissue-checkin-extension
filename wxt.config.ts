47c0a79c00b994b9c95fca9d8ee9b123a1d200b7
Eai <eai@mizle.net>
2025-01-18 03:26:08 +0900

    fix: add extension id to manifest due to firefox
    https://extensionworkshop.com/documentation/develop/extensions-and-the-add-on-id/

--- wxt.config.ts
+++ wxt.config.ts
@@ -9,6 +9,11 @@ export default defineConfig({
             action: {
                 default_title: "Tissue Checkin",
             },
+            browser_specific_settings: {
+                gecko: {
+                    id: "{b3409509-ae59-42f0-aa62-76ff16ec9b61}",
+                },
+            },
         };
     },
 });
